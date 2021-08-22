2774431cef8fdd5c062631934e998ead6c9c2743
Vitor Gomes Soares Lins Peixoto <vitor.peixoto@eteg.com.br>
2021-08-22 17:27:55 -0300

    Added scripts to install and run the project

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Front-end
+git clone https://github.com/Galantine/front-end-cefetweb
+cd front-end-cefetweb && npm install && cd ..
+# Back-end
+git clone https://github.com/Galantine/back-end-cefetweb
+cd back-end-cefetweb && npm install && cd ..
---
+++ run.sh
@@ -0,0 +1,2 @@
+cd front-end-cefetweb && npm start && cd .. &
+cd back-end-cefetweb && npm start && cd .. &
