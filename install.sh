ab405bd7b0929f52a356bf5e399a3293e384bd27
LucasMarioza <lucasmarioza@gmail.com>
2021-08-29 20:21:40 -0300

    Fix Repo URLs

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Front-end
-git clone https://github.com/Galantine/front-end-cefetweb
+git clone git@github.com:Galantine/front-end-cefetweb.git
 cd front-end-cefetweb && npm install && cd ..
 # Back-end
-git clone https://github.com/Galantine/back-end-cefetweb
+git clone git@github.com:Galantine/back-end-cefetweb.git
 cd back-end-cefetweb && npm install && cd ..
